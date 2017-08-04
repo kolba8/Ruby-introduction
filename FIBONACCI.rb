81e0c4c2eae1b4e6d9e27e0d3ecad6e3eb874716
Amadeusz Kolbusz <kolba888@interia.pl>
2017-08-04 10:55:21 +0200

    Fibonacci

--- FIBONACCI.rb
+++ FIBONACCI.rb
@@ -8,4 +8,21 @@ elsif ! (ARGV[0].chars.all? { |c| "0123456789".include?(c) })
   exit
 end
 
-n = ARGV[0]
+n = ARGV[0].to_i
+v = 0
+
+def fibonacci(x)
+if x == 0
+  f = 0
+elsif x == 1
+  f = 1
+elsif
+f = fibonacci(x-2) + fibonacci(x-1)
+end
+return f
+end
+
+while v < n
+  puts fibonacci(v)
+  v += 1
+end
